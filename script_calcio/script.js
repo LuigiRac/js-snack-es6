5602b9377d838ac971424565ea990dd8b3ac2b2c
LuigiRac <lui.rac06@gmail.com>
2024-11-13 22:42:19 +0100

    separati i due esercizi in due cartelle diverse

---
+++ script_calcio/script.js
@@ -0,0 +1,17 @@
+"use strict"
+console.clear();
+
+/* Creare un array di oggetti di squadre di calcio.
+ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+let squadreCalcio = [
+    { nome: "Fiorentina", punti: 0, falli: 0 },
+    { nome: "Milan", punti: 0, falli: 0 },
+    { nome: "Juventus", punti: 0, falli: 0 },
+    { nome: "Inter", punti: 0, falli: 0 },
+    { nome: "Roma", punti: 0, falli: 0 },
+]
+
